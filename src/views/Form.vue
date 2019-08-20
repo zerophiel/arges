e59e6f6c703b3a165f8ebfb836b62eccaac9c34d
danuarta.wiratama <danuarta.wiratama@gdn-commerce.com>
2019-08-20 14:40:34 +0700

    Updating to now

---
+++ src/views/Form.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+      <v-flex
+        xs12
+        >
+        <v-card>
+          <v-card-title>
+            Affected Apps
+            <v-spacer></v-spacer>
+            <v-text-field
+                    v-model="search"
+                    append-icon="mdi-magnify"
+                    label="Search"
+                    single-line
+                    hide-details
+            ></v-text-field>
+          </v-card-title>
+          <v-data-table
+                  :headers="headers"
+                  :items="affectedApp"
+                  :search="search"
+          ></v-data-table>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    search: "",
+    headers: [
+      {
+        text: 'ID',
+        align: 'center',
+        value: 'affectedAppId',
+      },
+      {
+        text: 'Label',
+        align: 'center',
+        value: 'affectedAppLabel',
+      },
+    ],
+    affectedApp: []
+  }),
+  mounted() {
+    this.getAllAffectedApp()
+  },
+  methods: {
+    getAllAffectedApp: function(){
+      this.$http.get("https://pedas.gdn-app.com/hermes/affected-apps/")
+              .then(response => {
+                for(let i = 0; i < response.data.data.length; i++){
+                  this.affectedApp.push({
+                    id: response.data.data[i].ID,
+                    affectedAppId: response.data.data[i].Code,
+                    affectedAppLabel: response.data.data[i].Name
+                  })
+                }
+              })
+              .catch(e => {
+                console.log(e)
+              })
+    },
+  }
+};
+</script>
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+      <v-flex
+        xs12
+        >
+
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+
+  }),
+  mounted() {
+
+  },
+  methods: {
+
+  }
+};
+</script>
